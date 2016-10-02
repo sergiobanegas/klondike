8081289052ed8c5d6a8a53a60297977534c10e0f
sergiobanegas <sbanegas92@gmail.com>
2016-10-02 16:27:33 +0200

    Create Exit option

---
+++ src/main/java/es/upm/miw/iwvg/klondike/views/ExitView.java
@@ -0,0 +1,20 @@
+package es.upm.miw.iwvg.klondike.views;
+
+import es.upm.miw.iwvg.klondike.controllers.ExitController;
+import es.upm.miw.iwvg.klondike.utils.IO;
+
+public class ExitView {
+
+    public void interact(ExitController controller) {
+        IO io = new IO();
+        int answer = io.readInt("¿Estás seguro de terminar el juego? (0-no, 1-sí)");
+        while ((answer!=0) && (answer!=1)){
+            io.writeln("Por favor, introduzca un número válido.");
+            io.readInt("¿Estás seguro de terminar el juego? (0-no, 1-sí)");
+        }
+        if (answer==1){
+            io.writeln("Adios!!!");
+            controller.control();
+        } 
+    }
+}
